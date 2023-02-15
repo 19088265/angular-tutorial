9d992b385e1397ac0e65c948e5e899e40a480a94
19088265 <103127317+19088265@users.noreply.github.com>
2023-02-15 17:53:46 +0200

    2 files added

---
+++ src/app/cart.service.ts
@@ -0,0 +1,29 @@
+import { Product } from './products';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  //items property to store an array of products currently in the cart
+  items: Product[] = [];
+
+  //Add current product to items array
+  addToCart(product: Product){
+    this.items.push(product);
+  }
+
+  //Retrieve products from items array
+  getItems(){
+    return this.items;
+  }
+
+  //Clear cart by declaring current array as empty
+  clearCart(){
+    this.items = [];
+    return this.items;
+  }
+
+  
+}
