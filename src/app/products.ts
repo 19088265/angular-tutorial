502491f3eb8dee1053029c79bfaa43d52c542513
19088265 <103127317+19088265@users.noreply.github.com>
2023-02-15 18:11:22 +0200

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,24 +8,23 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'Samsung S23 Ultra',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
   },
   {
     id: 2,
-    name: 'Phone Mini',
+    name: 'IPhone 14 Pro Max',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: 'Huawei Y7P',
     price: 299,
-    description: ''
-  }
+    description: '',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
