56b02fa0a96cd749cc17fb0efb8ee75e1c04a7a9
19088265 <103127317+19088265@users.noreply.github.com>
2023-02-15 23:21:26 +0200

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -12,8 +13,20 @@ export class CartComponent{
   //Use method in cart service to obtain items
   items = this.cartService.getItems();
 
+  //Gather user's name and address
+  checkoutForm = this.formBuilder.group({
+    name: '', address: ''
+  });
+
   //Inject cart service so that cartComp can use it
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private formBuilder: FormBuilder,) {}
+
+  //Submit method to process the form
+  onSubmit():void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 
   
 }
