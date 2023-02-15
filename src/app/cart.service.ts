bb0195fa7efed4cebdc553d87c6bcf0b914f48d5
19088265 <103127317+19088265@users.noreply.github.com>
2023-02-15 19:17:26 +0200

    3 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,28 +2,30 @@ import { Product } from './products';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
   //items property to store an array of products currently in the cart
   items: Product[] = [];
 
   //Add current product to items array
-  addToCart(product: Product){
+  addToCart(product: Product) {
     this.items.push(product);
   }
 
+  removeItem(product: Product) {
+    let index = this.items.indexOf(product);
+    this.items.splice(index, 0);
+  }
+
   //Retrieve products from items array
-  getItems(){
+  getItems() {
     return this.items;
   }
 
   //Clear cart by declaring current array as empty
-  clearCart(){
+  clearCart() {
     this.items = [];
     return this.items;
   }
-
-  
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-cart',
@@ -7,5 +8,14 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+
+  //Use method in cart service to obtain items
+  items = this.cartService.getItems();
+
+  //Inject cart service so that cartComp can use it
   constructor(private cartService: CartService) {}
+
+  removeItem(product: Product){
+    this.cartService.removeItem(product);
+  }
 }
