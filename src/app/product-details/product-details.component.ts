553b82b119107863bb959d144387ab809906eeaa
19088265 <103127317+19088265@users.noreply.github.com>
2023-02-15 15:12:14 +0200

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -17,7 +17,7 @@ implements OnInit {
 ngOnInit() {
   //First get the product id from the current route
   const routeParams = this.route.snapshot.paramMap;
-  const productIdFromRoute = Number(routeParams.get('product.id'));
+  const productIdFromRoute = Number(routeParams.get('productId'));
 
   //Find the product that corresponds with the id provided in this route
   this.product = products.find(product => product.id === productIdFromRoute);
