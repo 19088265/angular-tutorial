1de8335858e3195b4e84b587c5f1d41de876415d
19088265 <103127317+19088265@users.noreply.github.com>
2023-02-16 22:03:46 +0200

    3 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -3,15 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+  styleUrls: ['./top-bar.component.css'],
 })
 export class TopBarComponent {
-  checkout(){
-    
+  //Change checkout button text when clicked
+  checkout() {
+    const checkBtn = document.querySelector('#btnCheck') as HTMLDivElement;
+    checkBtn.style.color = 'white';
   }
-
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
